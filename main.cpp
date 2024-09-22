45f42cabc481ad8c0d7e3c24259c3ee2f9e580ee
Ани Арутюнян <arutiunianan@icloud.com>
2024-09-22 23:47:51 +0300

    fix main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "cache/lfu_cache.h"
 #include "cache/pca_cache.h"
-#include "graph/graph.h"
+#ifndef OPTIMIZATION
+    #include "graph/graph.h"
+#endif
 
 int main() {
     int max_size;
@@ -24,19 +26,25 @@ int main() {
         }
     #endif
 
-    Graph graph;
+    #ifndef OPTIMIZATION
+        Graph graph;
+    #endif
     LFU_cache_t<int> lfu(max_size, num_of_num, num);
     PCA_cache_t<int> pca(max_size, num_of_num, num);
     for(int i = 0; i < num_of_num; i++) {
         lfu.lookup_update(num[i]);
         pca.lookup_update(num, i);
 
-        graph.add_hits(lfu.hits_counter, pca.hits_counter);
+        #ifndef OPTIMIZATION
+            graph.add_hits(lfu.hits_counter, pca.hits_counter);
+        #endif
     }
 
     std::cout << "\nlfu: " << lfu.hits_counter << "\n";     
     std::cout << "\npca: " << pca.hits_counter << "\n\n";
-    graph.print_graph();
+    #ifndef OPTIMIZATION
+        graph.print_graph();
+    #endif
 
     return 0;
 }
